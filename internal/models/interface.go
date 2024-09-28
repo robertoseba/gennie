b7ae0e8e6dd381d57ed3b8490282e4cf071bd12c
Roberto Seba <roberto@robertoseba.com>
2024-09-28 12:55:08 -0300

    doc: todos

--- internal/models/interface.go
+++ internal/models/interface.go
@@ -6,6 +6,11 @@ import (
 )
 
 type IModel interface {
+	//TODO: remove profile dependency and use only the profile data needed. Call it system prompt
+	//TODO: should model receive a chat history and question or a chat history prepare for next question
+	//TODO: Ask would then be replaced by CompleteChat where the model would receive
+	// the chat history and fillout the next answer.
+	// In this scenario, chatHistory should also have profile.Data instead of being sent to the model.
 	Ask(question string, profile *profile.Profile, history *chat.ChatHistory) (*chat.Chat, error)
 	Model() string
 }
