cb77748db193394aad0dde31a249c33f26783b84
Roberto Seba <robertogseba@gmail.com>
2024-12-04 14:29:39 -0300

    test: still setting up test

--- internal/core/usecases/get_answer_test.go
+++ internal/core/usecases/get_answer_test.go
@@ -7,20 +7,17 @@ import (
 func TestGetAnswerService(t *testing.T) {
 	//TODO: create mock IAPIclient
 	t.Run("completes the conversation with answers from the API", func(t *testing.T) {
-	})
-
-	t.Run("returns an error if cant find profile", func(t *testing.T) {
-	})
-
-	t.Run("returns an error if cant find model", func(t *testing.T) {
+		// repo.AssertExpectations(t)
 	})
 
 	t.Run("appends the content of a file to the conversation", func(t *testing.T) {
 	})
 
-	t.Run("returns an error if the conversation cannot be loaded", func(t *testing.T) {
+	t.Run("if model not provided, uses the model from the active conversation", func(t *testing.T) {
 	})
 
+	t.Run("if profile not provided, uses the profile from the active conversation", func(t *testing.T) {
+	})
 	t.Run("when model is inputed replaces the model in active conversation", func(t *testing.T) {
 	})
 
@@ -31,5 +28,15 @@ func TestGetAnswerService(t *testing.T) {
 	})
 
 	t.Run("when input is a follow up question, appends the question to the conversation", func(t *testing.T) {
+	})
+
+	//Error Handling
+	t.Run("returns an error if cant find profile", func(t *testing.T) {
+	})
+
+	t.Run("returns an error if cant find model", func(t *testing.T) {
+	})
+
+	t.Run("returns an error if cant load active conversation", func(t *testing.T) {
 	})
 }
