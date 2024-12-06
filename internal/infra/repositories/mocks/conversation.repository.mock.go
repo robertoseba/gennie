d10e50dd4e17d2f6356422caa95a11e370e2bc4c
Roberto Seba <robertogseba@gmail.com>
2024-12-06 15:28:05 -0300

    test: creating mocks for testing

--- internal/infra/repositories/mocks/conversation.repository.mock.go
+++ internal/infra/repositories/mocks/conversation.repository.mock.go
@@ -19,7 +19,7 @@ func (m *MockConversationRepository) LoadActive() (*conversation.Conversation, e
 }
 
 func (m *MockConversationRepository) SaveAsActive(conv *conversation.Conversation) error {
-	args := m.Called(conv)
+	args := m.Called(conv.LastQuestion(), conv.LastAnswer())
 	return args.Error(0)
 }
 func (m *MockConversationRepository) ExportToFile(conv *conversation.Conversation, filename string) error {
---
+++ internal/infra/repositories/mocks/profile.repository.mock.go
@@ -0,0 +1,24 @@
+package mocks
+
+import (
+	"github.com/robertoseba/gennie/internal/core/profile"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockProfileRepository struct {
+	mock.Mock
+}
+
+func NewMockProfileRepository() *MockProfileRepository {
+	return &MockProfileRepository{}
+}
+
+func (m *MockProfileRepository) FindBySlug(slug string) (*profile.Profile, error) {
+	args := m.Called(slug)
+	return args.Get(0).(*profile.Profile), args.Error(1)
+}
+
+func (m *MockProfileRepository) ListAll() (map[string]*profile.Profile, error) {
+	args := m.Called()
+	return args.Get(0).(map[string]*profile.Profile), args.Error(1)
+}
